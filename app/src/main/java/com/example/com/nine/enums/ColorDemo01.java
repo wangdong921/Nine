2f223f3ba94a0e9d2ead9e285c15f73831e7a4e9
wangdong <873889098@qq.com>
2016-07-30 16:57:56 +0800

    123

---
+++ app/src/main/java/com/example/com/nine/enums/ColorDemo01.java
@@ -0,0 +1,20 @@
+package com.example.com.nine.enums;
+
+/**
+ * Created by 汪栋 on 2016/7/29.
+ */
+
+enum Color{
+    BLUE;
+}
+
+public class ColorDemo01 {
+
+
+    public static void main(String args[]){
+        Color c = Color.BLUE;
+        System.out.println(c);
+
+    }
+
+}
